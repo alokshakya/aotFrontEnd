1bd7d0a61ef0fbb5585ff343a8177e95c0239637
Gaurav Pant <gp@po.com>
2017-04-08 11:59:12 +0530

    Resolves issue#6, #7

--- src/app/accountsettings/accountsettings.component.ts
+++ src/app/accountsettings/accountsettings.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsettingsComponent implements OnInit {
 
+  data: any = [
 
+              {"Order ID":"OB26DF", "Subject":"Science", "Subscription Date":"23 March 2017", "Valid Till":"23 September 2017"},
+              {"Order ID":"OBH8R3", "Subject":"Reasoning", "Subscription Date":"02 April 2017", "Valid Till":"02 October 2017"},
+              {"Order ID":"OBTDE3", "Subject":"Mathematics", "Subscription Date":"05 June 2017", "Valid Till":"05 January 2018"}
   
+              ]
+  
+
 
   constructor() { }
 
