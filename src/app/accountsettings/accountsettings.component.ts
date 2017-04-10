b90c7c599587c3d15771a29f3fc71587b33baab9
Gaurav Pant <gp@po.com>
2017-04-10 18:10:46 +0530

    added global filter and paginator

--- src/app/accountsettings/accountsettings.component.ts
+++ src/app/accountsettings/accountsettings.component.ts
@@ -11,7 +11,12 @@ export class AccountsettingsComponent implements OnInit {
 
               {"Order ID":"OB26DF", "Subject":"Science", "Subscription Date":"23 March 2017", "Valid Till":"23 September 2017"},
               {"Order ID":"OBH8R3", "Subject":"Reasoning", "Subscription Date":"02 April 2017", "Valid Till":"02 October 2017"},
-              {"Order ID":"OBTDE3", "Subject":"Mathematics", "Subscription Date":"05 June 2017", "Valid Till":"05 January 2018"}
+              {"Order ID":"OBTDE4", "Subject":"Mathematics", "Subscription Date":"05 June 2017", "Valid Till":"05 December 2017"},
+              {"Order ID":"OBTDZ2", "Subject":"Mathematics", "Subscription Date":"05 June 2017", "Valid Till":"05 December 2017"},
+              {"Order ID":"OBDJJ4", "Subject":"English", "Subscription Date":"05 June 2017", "Valid Till":"05 January 2018"},
+              {"Order ID":"OBSKD3", "Subject":"Reasoning", "Subscription Date":"05 March 2017", "Valid Till":"05 January 2018"},
+              {"Order ID":"OBKG33", "Subject":"General Knowledge", "Subscription Date":"05 April 2017", "Valid Till":"05 January 2018"},
+              {"Order ID":"OBHT45", "Subject":"Computer/Cyber", "Subscription Date":"05 June 2017", "Valid Till":"05 January 2018"},
   
               ]
   
