ea5a94c7e2fe5800ec2db65a5f4a2c8b14a6db25
Gaurav Pant <gp@po.com>
2017-04-10 16:12:36 +0530

    fixed footer

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -5,8 +5,8 @@ import {AppComponent} from './app.component';
     selector: 'app-footer',
     template: `
         <div class="footer">
-            <div class="card clearfix">
-                <span class="footer-text-left">OlympiadBox</span>
+            <div class="card clearfix" style="background-color:#2D353C">
+                <span class="footer-text-left" style="color:#8A8C8D">OlympiadBox</span>
                 <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
             </div>
         </div>
