807750b1cd11ec471e32018c1644f956d5f33220
Gaurav Pant <gp@po.com>
2017-04-11 11:10:56 +0530

    changed footer text color

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -6,8 +6,7 @@ import {AppComponent} from './app.component';
     template: `
         <div class="footer">
             <div class="card clearfix" style="background-color:#2D353C">
-                <span class="footer-text-left" style="color:#8A8C8D">OlympiadBox</span>
-                <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
+                <span class="footer-text-left" style="color:white">Copyright &copy; 2017 OlympiadBox. All Rights Reserved </span>
             </div>
         </div>
     `
