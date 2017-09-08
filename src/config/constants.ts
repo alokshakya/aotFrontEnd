42da4320655379256a1d4a7e5dd7cd038537a7c9
Gaurav Pant <gp@po.com>
2017-09-08 10:21:36 +0530

    changed base url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const OLYMPIADBOX_INSTANCE_URL: string = 'http://scripts.olympiadbox.com/services/backy-api';
+export const OLYMPIADBOX_INSTANCE_URL: string = 'http://scripts.olympiadbox.com/services/fronty-api';
 // export const OLYMPIADBOX_INSTANCE_URL: string = 'http://localhost/backy-api';
 
 export const OLYMPIADBOX_API_KEY: string = '36fda24fe5588fa4cdf23xxss226c6c2fdfbdb6b6bc7864699774c9jh790f706d05a88';
