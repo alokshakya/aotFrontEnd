c3847e89830e8ea4b14ca3cd3a965934f789d13e
Gaurav Pant <gp@po.com>
2017-10-02 10:32:18 +0530

    issue #16 , removed class 9 and 10

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -98,7 +98,7 @@ export class LoginComponent implements OnInit {
         this.dummyClass = []
         this.dummyClass.push({ label: "Select Class", value: null }, { label: "I", value: "I" },
             { label: "II", value: "II" }, { label: "III", value: "III" }, { label: "IV", value: "IV" }, { label: "V", value: "V" },
-            { label: "VI", value: "VI" }, { label: "VII", value: "VII" }, { label: "VIII", value: "VIII" }, { label: "IX", value: "IX" }, { label: "X", value: "X" })
+            { label: "VI", value: "VI" }, { label: "VII", value: "VII" }, { label: "VIII", value: "VIII" })
         this.reset();
     
     }
