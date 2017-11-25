aa741dc81a32dccc8db9a7bf04b8971e14bbb388
Gaurav Pant <gp@po.com>
2017-11-25 12:24:21 +0530

    issue #64

--- src/app/components/account/mathematics/sampletest/sampletest-math.component.ts
+++ src/app/components/account/mathematics/sampletest/sampletest-math.component.ts
@@ -3,7 +3,9 @@ import { SelectItem } from 'primeng/primeng';
 import { Response } from '@angular/http';
 import { TreeModule, TreeNode } from 'primeng/primeng';
 import { Router } from '@angular/router';
-import { SubjectInfo, Misc } from '../../../../services/data.service';
+import { Message } from 'primeng/primeng';
+import { PersonalInfo, SubjectInfo, Misc, chapterwiseTest } from '../../../../services/data.service';
+import { MasterHttpService } from '../../../../services/masterhttp.service'
 
 @Component({
   selector: 'app-sampletest-math',
@@ -12,8 +14,9 @@ import { SubjectInfo, Misc } from '../../../../services/data.service';
 })
 export class SampletestMathComponent implements OnInit {
     examPattern: SelectItem[];
-
+    spinner2;
     testData: any;
+    generateMsg:Message[];
 
     sampleTestData: any; //for chart
 
@@ -24,7 +27,10 @@ export class SampletestMathComponent implements OnInit {
     constructor(
         private router: Router,
         private misc: Misc,
-        public subjectInfo: SubjectInfo) {
+        private masterhttp:MasterHttpService,
+        public subjectInfo: SubjectInfo,
+        public test:chapterwiseTest,
+        public personalInfo:PersonalInfo) {
 
         this.testData = {
             "Sample Test 1": "35/50",
@@ -42,6 +48,12 @@ export class SampletestMathComponent implements OnInit {
         this.examPattern = [];
         this.examPattern.push({ label: "SOF", value: "null" }, { label: "SELECT EXAM", value: "null" }, )
 
+    }
+    shade(index){
+        if(index==0||index==2||index==4){
+            return 'dark';
+        }
+        return 'light';
     }
 
     ngOnInit() {
@@ -86,6 +98,36 @@ export class SampletestMathComponent implements OnInit {
         //   }
         // })
 
+    }
+
+    startTest(testId, chapterId, attempted, completed, chapter) {
+    this.spinner2 = testId;
+    let wrapper = {
+        "student_id": this.personalInfo.studentInfo['student_id'],
+        "chapter_id": chapterId,
+        "test_id": testId,
+        "attempt":attempted,
+        "completed":completed.toString()
+    }
+    this.test.activateTestRoute();
+    this.test.setSubject('Mathematics',chapter);
+    this.masterhttp.beginTest(wrapper)
+    .subscribe((data) => {
+            if (data['status'] == 200){
+                this.test.setQuesAnswerSet(data['message']);
+                this.router.navigate(['test']);
+            }else{
+                this.generateMsg = [];
+                this.generateMsg.push({ severity: 'info', summary: 'Could Not Start Test', detail: 'Please Try Again'});
+            }
+            this.spinner2 = null;
+
+        },
+        err => {
+                this.generateMsg = [];
+                this.generateMsg.push({ severity: 'info', summary: 'Could Not Start Test', detail: 'Please Try Again'});
+                this.spinner2 = null;
+        });
     }
 
     redirect() {
