60f90126ca7bdceab449db68957ad6702c40ee67
alokshakya <alokshakyacode@gmail.com>
2019-08-18 16:46:59 +0530

    science routing module defined and implemented

--- src/app/components/account/science/science-routing.module.ts
+++ src/app/components/account/science/science-routing.module.ts
@@ -7,7 +7,6 @@ import { MocktestScienceComponent } from './mocktest/mocktest-science.component'
 import { DemotestScienceComponent } from './demotest/demotest-science.component';
 import { ChapterwisetestScienceComponent } from './chapterwisetest/chapterwisetest-science.component';
 
-
 const routes: Routes = [
   {path:'demotest', component: DemotestScienceComponent},
   {path:'chapterwisetest', component: ChapterwisetestScienceComponent},
